bb20d14c4a7969451e0498cbf18383410c5a19d1
timbe16 <tabelopa@gmail.com>
2017-09-13 14:32:53 +0300

    1

--- app/api/mail.py
+++ app/api/mail.py
@@ -3,9 +3,12 @@ from .api_helper import *
 from app.util.authentication import auth_token_required, ERROR_LEVEL_API
 from app.model.email import Email, EmailStatus
 from app.util.db import db
+from app.util.smtp import SMTP
+from sqlalchemy import desc
 import smtplib
 import bleach
 import json
+import sys
 
 mail_api = Blueprint("mail_api", __name__, url_prefix='/api/mail')
 
@@ -22,7 +25,7 @@ def sendmail():
     :rtype: json
     """
 
-    response_dictionary = {}
+    response_dictionary = {'status_code': 200}
     content = request.get_json(force=True)
     mail_from = required_param(content, "from", str)
     mail_to = required_param(content, "to", str)
@@ -31,28 +34,26 @@ def sendmail():
     mail_subj = bleach.clean(optional_param(content, "subject", str))
     mail_body = bleach.clean(optional_param(content, "text", str))
     mailto = [mail_to]
-    if not mail_cc:
+    if mail_cc is not None:
         mailto += [mail_cc]
-    if not mail_bcc:
+    else:
+        mail_cc = ""
+    if mail_bcc is not None:
         mailto += [mail_bcc]
+    else:
+        mail_bcc = ""
 
     # Prepare message
     message = """From: %s\nTo: %s\nCc: %s\nBcc: %s\nSubject: %s\n\n%s""" % (mail_from, mail_to, mail_cc, mail_bcc, mail_subj, mail_body)
     try:
-        server = smtplib.SMTP(app.config['SMTP_HOST'], app.config['SMTP_PORT'])
-        server.ehlo()
-        server.starttls()
-        server.login(app.config['SMTP_USER'], app.config['SMTP_PASSWD'])
-        response = server.sendmail(mail_from, mailto, message)
-        print "response"
+        mailer = SMTP(host=app.config['SMTP_HOST'], port=app.config['SMTP_PORT'], user=app.config['SMTP_USER'], password=app.config['SMTP_PASSWD'])
+        response = mailer.send_message(mail_from, mailto, message)
+        print "response:"
         print response
-        print type(response)
-        server.close()
-        response_dictionary['status_code'] = 200
+        # response = {"err": 1}
+        if response is False:
+            response_dictionary['status_code'] = 500
     except:
-        response_dictionary['status_code'] = 500
-
-    if len(response) > 0:
         response_dictionary['status_code'] = 500
 
     mail = Email(mail_from=mail_from,
@@ -73,15 +74,19 @@ def sendmail():
     return jsonify(**response_dictionary)
 
 
-@mail_api.route('/get_status', methods=['POST'])
+@mail_api.route('/get_status/<int:id>', methods=['GET'])
 @auth_token_required
-def get_status():
+def get_status(id):
     """Get email delivery status.
 
     .. :quickref: Get email delivery status
 
-    :param json (email_id):
-    :returns: int status_code
+    :param int (email_id):
+    :returns: bool status
     :rtype: json
     """
-    pass
+    response_dictionary = {}
+    mail = EmailStatus.query.filter(EmailStatus.email_id == id).order_by(desc(EmailStatus.sent_at)).first()
+    response_dictionary['status'] = mail.delivered
+
+    return jsonify(**response_dictionary)
---
+++ app/util/smtp.py
@@ -0,0 +1,39 @@
+import smtplib
+
+
+class SMTP():
+    def __init__(self, host, port, user, password):
+        self.conn = None
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+    def open(self):
+        if self.conn:
+            return False
+
+        self.conn = smtplib.SMTP(self.host, self.port)
+        self.conn.ehlo()
+        self.conn.starttls()
+        self.conn.login(self.user, self.password)
+
+        return self.conn
+
+    def close(self):
+        if self.conn:
+            return self.conn.quit()
+        return None
+
+    def send_message(self, mail_from, mailto, message):
+        if not mail_from or not mailto or not message:
+            return False
+
+        new_conn = self.open()
+
+        response = self.conn.sendmail(mail_from, mailto, message)
+
+        if new_conn:
+            self.close()
+
+        return response
