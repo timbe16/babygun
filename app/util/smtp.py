b7afac24231d0c4c0baa6bb3ed487156530fc681
timbe16 <tabelopa@gmail.com>
2017-09-16 00:31:14 +0300

    smtp connection closing fixed

--- app/util/smtp.py
+++ app/util/smtp.py
@@ -1,35 +1,29 @@
 import smtplib
+import sys
+from six import reraise as raise_
 
 
 class SMTP(object):
-    def __init__(self, host, port, user, password):
+    def __init__(self, host, port, user, password, timeout = 5):
         self.conn = None
         self.host = host
         self.port = port
         self.user = user
         self.password = password
-
-    def open(self):
-        if self.conn:
-            return self.conn
-
-        self.conn = smtplib.SMTP(self.host, self.port)
-        self.conn.ehlo()
-        self.conn.starttls()
-        self.conn.login(self.user, self.password)
-
-        return self.conn
-
-    def close(self):
-        if self.conn is not None:
-            self.conn.quit()
-            self.conn = None
-
-        return None
+        self.timeout = timeout
 
     def send_message(self, mail_from, mailto, message):
-        self.open()
-        response = self.conn.sendmail(mail_from, mailto, message)
-        self.close()
+        try:
+            self.conn = smtplib.SMTP(host=self.host, port=self.port, timeout=self.timeout)
+            self.conn.ehlo()
+            self.conn.starttls()
+            self.conn.login(self.user, self.password)
+            response = self.conn.sendmail(mail_from, mailto, message)
+        except Exception as e:
+            traceback = sys.exc_info()[2]
+            raise_(Exception, str(e), traceback)
+        finally:
+            if self.conn is not None:
+                self.conn.quit()
 
         return response
