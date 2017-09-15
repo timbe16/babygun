92f75c86cc8da6b7e4076b00a4333da021d0e73b
timbe16 <tabelopa@gmail.com>
2017-09-15 16:01:52 +0300

    tests fixed

--- testapi.py
+++ testapi.py
@@ -1,19 +1,18 @@
 import unittest
 import requests
-import json
-import re
 from app import app
 
 
-class TestMailApi(unittest.TestCase):
+class TestMailApiSendMessage(unittest.TestCase):
     def test_sendmail(self):
         response = requests.post('http://localhost:5000/api/mail/send',
                                  headers={'Client-Key': app.config["CLIENT_KEY"], 'Content-Type': 'application/json'},
                                  json={"to": app.config["SMTP_USER"],
                                        "from": app.config["SMTP_USER"],
-                                       "subject": "test mail",
+                                       "subject": "TestMailApiSendMessage",
                                        "text": "test"})
-        self.assertTrue(response.text.find('"status_code": 200'))
+        r = response.json()
+        self.assertTrue(r["status_code"] == 200)
 
 
 if __name__ == "__main__":
